58373aa30be3e6851144154909e5d9c2752f71c3
Edward Stoner <edwardlstoner@gmail.com>
2020-04-22 09:42:23 -0600

    Add keys

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -18,11 +18,11 @@ class Orders extends Component {
   render() {
     const orderEls = this.props.orders.map((order) => {
       return (
-        <div className="order">
+        <div key={order.id} className="order">
           <h3>{order.name}</h3>
           <ul className="ingredient-list">
-            {order.ingredients.map((ingredient) => {
-              return <li>{ingredient}</li>;
+            {order.ingredients.map((ingredient, i) => {
+              return <li key={i}>{ingredient}</li>;
             })}
           </ul>
         </div>
