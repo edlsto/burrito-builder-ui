14a11bc84d1a632931f08ce344aa756586fef1fa
Edward Stoner <edwardlstoner@gmail.com>
2020-04-22 10:08:22 -0600

    Add add order action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
-export const setOrders = orders => ({
-  type: 'SET_ORDERS',
-  orders
+export const setOrders = (orders) => ({
+  type: "SET_ORDERS",
+  orders,
+});
+
+export const addOrder = (order) => ({
+  type: "ADD_ORDER",
+  order,
 });
