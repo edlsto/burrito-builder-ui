d81f55221fca474c1bec76535948ea7f0234c158
Edward Stoner <edwardlstoner@gmail.com>
2020-04-22 10:08:03 -0600

    Add postNewOrder

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,18 @@
 export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
-}
+  return fetch("http://localhost:3001/api/v1/orders").then((response) =>
+    response.json()
+  );
+};
+
+export const postNewOrder = (order) => {
+  return fetch("http://localhost:3001/api/v1/orders", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: order.name,
+      ingredients: order.ingredients,
+    }),
+  }).then((response) => response.json());
+};
