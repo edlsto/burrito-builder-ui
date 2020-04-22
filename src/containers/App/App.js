f6796e9ee3d98105d4caa24cbf652add84d6c56b
Edward Stoner <edwardlstoner@gmail.com>
2020-04-22 09:30:39 -0600

    Turn App into functional component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,45 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import { setOrders } from '../../actions';
-import { getOrders } from '../../apiCalls';
-import Orders from '../../components/Orders/Orders';
-import OrderForm from '../../components/OrderForm/OrderForm';
+import React from "react";
+import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super();
-    this.props = props;
-  }
+import Orders from "../../components/Orders/Orders";
+import OrderForm from "../../components/OrderForm/OrderForm";
 
-  componentDidMount() {
-    getOrders()
-      .then(data => this.props.setOrders(data.orders))
-      .catch(err => console.error('Error fetching:', err));
-  }
+const App = () => {
+  return (
+    <main className="App">
+      <header>
+        <h1>Burrito Builder</h1>
+        <OrderForm />
+      </header>
 
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
+      <Orders />
+    </main>
+  );
+};
 
-        <Orders orders={this.props.orders}/>
-      </main>
-    );
-  }
-}
-
-const mapStateToProps = ({ orders }) => ({
-  orders
-});
-
-const mapDispatchToProps = dispatch => (
-  {
-    setOrders: (orders) => dispatch(setOrders(orders))
-  }
-);
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
